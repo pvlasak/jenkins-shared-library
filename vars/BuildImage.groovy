ba7bb203c7dc907307e5a3ddfaab6d706939bed8
pvlasak <93707814+pvlasak@users.noreply.github.com>
2026-04-02 09:46:15 +0200

    Delete vars directory

--- vars/BuildImage.groovy
+++
@@ -1,9 +0,0 @@
-#!/user/bin/env groovy
-def call(String ImageName) {
-    echo("building the docker image...")
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
-        sh "docker build -t $ImageName ."
-        sh 'echo $PASSWORD | docker login -u $USER --password-stdin'
-        sh "docker push $ImageName"
-    }
-}
--- vars/BuildJar.groovy
+++
@@ -1,5 +0,0 @@
-#!/user/bin/env groovy
-def call() {
-    echo "building the application...building the application for $GIT_BRANCH"
-    sh "mvn package"
-}
